96f9ef5ecd806866bdb45ddc2650070bdfc323ca
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-06-16 14:20:57 -0500

    lineNumbering complete

--- lineNumbering/lineNumbering.js
+++ lineNumbering/lineNumbering.js
@@ -1,3 +1,18 @@
 //Write a function which takes a list of strings and
 //returns each line prepended by the correct number.
 
+var number=function(array){
+  if (array.length === 0) {
+    return [];
+  }
+
+  var result = [];
+
+  for (var i = 0; i < array.length; i ++) {
+    var string = "";
+    string += (i + 1) + ": " + array[i];
+    result.push(string);
+  }
+
+  return result;
+}
